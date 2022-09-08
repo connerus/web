d5a9c6ba6dfc0eba7fbc5ee183ae0d9b300ef4e0
connerus <demowordchannel@gmail.com>
2022-09-09 00:14:58 +0600

    A bit of changes

--- scripts/script.js
+++ scripts/script.js
@@ -5,9 +5,16 @@ window.addEventListener("mouseover", (event) => {
 }
 )
 
-window.addEventListener("mouseout", (event) =>    {
+window.addEventListener("mouseout", (event) => {
     if (event.target.className == "menu-item"){
         event.target.style.backgroundColor = "black";
      }
+}
+)
+
+window.addEventListener("click", (event) => {
+   if (event.target.className == "menu-item"){
+      console.log("Yes")
+   }
 }
 )
