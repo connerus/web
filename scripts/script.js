528711ffe375bb25585f66ac9d81cca3b832b5e6
connerus <demowordchannel@gmail.com>
2022-11-21 23:08:41 +0600

    new things

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,9 @@
+const header = document.querySelector(".header-text")
+const about_btn = document.querySelector("#about")
+const skills_btn = document.querySelector("#skills")
+const socials_btn = document.querySelector("#socials")
+const header_text = document.querySelector(".header-info")
+
 window.addEventListener("mouseover", (event) => {
      if (event.target.className == "menu-item"){
         event.target.style.backgroundColor = "blue";
@@ -12,9 +18,19 @@ window.addEventListener("mouseout", (event) => {
 }
 )
 
-window.addEventListener("click", (event) => {
-   if (event.target.className == "menu-item"){
-      console.log("Yes")
-   }
+about_btn.addEventListener("click", (event) => {
+   header.innerText = "Who am I?"
+   header_text.innerText = "My name is Danil. "
 }
 )
+
+skills_btn.addEventListener("click", (event) => {
+   header.innerText = "My skills"
+}
+)
+
+socials_btn.addEventListener("click", (event) => {
+   header.innerText = "Social media"
+}
+)
+
